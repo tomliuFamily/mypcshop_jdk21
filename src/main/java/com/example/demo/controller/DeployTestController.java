636e329b061c594897f21389d55fedd6aa6040f2
tomliuFamily <abh9752@gmail.com>
2026-09-18 12:39:24 +0800

    修改 Version 成功版

--- src/main/java/com/example/demo/controller/DeployTestController.java
+++ src/main/java/com/example/demo/controller/DeployTestController.java
@@ -8,6 +8,6 @@ public class DeployTestController {
 
     @GetMapping("/api/deploy-test")
     public String deployTest() {
-        return "Jenkins CI/CD Version 5";
+        return "Jenkins CI/CD Version test successful!";
     }
 }
